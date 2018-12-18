0a48e1fcd655555e2831a9eb8b6edf5efd5d39b2
Filippo Ferrari <f.ferrari95@gmail.com>
2018-12-18 12:07:38 +0100

    html

--- sketch.js
+++ sketch.js
@@ -7,14 +7,14 @@ function preload(){
 }
 
 function setup() {
-  createCanvas(windowWidth,6000);
+  // createCanvas(windowWidth,3000);
 
 }
 
 function draw() {
-  push();
-  image(landing_gialla,0,0,landing_gialla.width,landing_gialla.height);
-  pop();
+  // push();
+  // image(landing_gialla,0,0,landing_gialla.width,landing_gialla.height);
+  // pop();
 }
 
 function windowResized() {
